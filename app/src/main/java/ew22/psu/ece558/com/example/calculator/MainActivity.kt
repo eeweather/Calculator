41f246257c346dd0c443e039b8198e2badaa383b
eweatherford <eweatherford@airsci.com>
2022-02-12 21:57:07 -0800

    Final edits and video

--- app/src/main/java/ew22/psu/ece558/com/example/calculator/MainActivity.kt
+++ app/src/main/java/ew22/psu/ece558/com/example/calculator/MainActivity.kt
@@ -15,16 +15,12 @@
 package ew22.psu.ece558.com.example.calculator
 
 // Import
+import android.content.Context
 import android.os.Bundle
 import android.os.Handler
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
-
-
-
-
-
 
 
 class MainActivity : AppCompatActivity() {
@@ -60,12 +56,10 @@ class MainActivity : AppCompatActivity() {
             showCat()
             handler.postDelayed({
                 // do something after 1000ms
-                                hideCat()
+                hideCat()
             }, 3000)
 
         }
-
-
 
         //listen for button click
         multButton.view.setOnClickListener {
@@ -73,8 +67,8 @@ class MainActivity : AppCompatActivity() {
             val ops = getOps()
             //check operands that need to be present are
             if (multButton.twoOpsIsEmpty(ops[0], ops[1]))
-                //if not pop up error text
-                Toast.makeText(this, "Please enter two operands", Toast.LENGTH_SHORT).show()
+            //if not pop up error text
+                ToastTwoOps(this)
             else {
                 //if present assign result text to the string version of the buttons result function
                 result.text = (multButton.getResult(
@@ -85,31 +79,30 @@ class MainActivity : AppCompatActivity() {
         }
 
         // all buttons follow same pattern
-        // really though about how I could loop through this but had trouble passing my button
-        // objects and then once that worked the context for the toast needed to be passed
-        // and that wasn't as straight forward as I thought it would be.
+        // really thought about how I could loop through this but had trouble passing my button
+        // objects and decided I liked practicing abstract classes more.
         // stuck with repeating on click logic for readability
         divButton.view.setOnClickListener {
 
-
             val ops = getOps()
-            if (divButton.twoOpsIsEmpty(ops[0], ops[1])){
-                Toast.makeText(this, "Please enter two operands", Toast.LENGTH_SHORT).show()
-            }
-            if (ops[1].text.toString().toDouble() == 0.0)
-                Toast.makeText(this, "Cannot divide by 0", Toast.LENGTH_SHORT).show()
-            else {
-                result.text = (divButton.getResult(
-                    ops[0].text.toString().toDouble(),
-                    ops[1].text.toString().toDouble()
-                )).toString()
+            if (divButton.twoOpsIsEmpty(ops[0], ops[1])) {
+                ToastTwoOps(this)
+            } else {
+                if (ops[1].text.toString().toDouble() == 0.0)
+                    Toast.makeText(this, getString(R.string.div_zero), Toast.LENGTH_SHORT).show()
+                else {
+                    result.text = (divButton.getResult(
+                        ops[0].text.toString().toDouble(),
+                        ops[1].text.toString().toDouble()
+                    )).toString()
+                }
             }
         }
 
         addButton.view.setOnClickListener {
             val ops = getOps()
             if (addButton.twoOpsIsEmpty(ops[0], ops[1]))
-                Toast.makeText(this, "Please enter two operands", Toast.LENGTH_SHORT).show()
+                ToastTwoOps(this)
             else {
                 result.text = (addButton.getResult(
                     ops[0].text.toString().toDouble(),
@@ -121,7 +114,7 @@ class MainActivity : AppCompatActivity() {
         subButton.view.setOnClickListener {
             val ops = getOps()
             if (subButton.twoOpsIsEmpty(ops[0], ops[1]))
-                Toast.makeText(this, "Please enter two operands", Toast.LENGTH_SHORT).show()
+                ToastTwoOps(this)
             else {
                 result.text = (subButton.getResult(
                     ops[0].text.toString().toDouble(),
@@ -133,7 +126,7 @@ class MainActivity : AppCompatActivity() {
         percButton.view.setOnClickListener {
             val ops = getOps()
             if (percButton.oneOpIsEmpty(ops[0]))
-                Toast.makeText(this, "Please enter value in operand 1", Toast.LENGTH_SHORT).show()
+                ToastOneOps(this)
             else {
                 result.text =
                     (percButton.getResult(ops[0].text.toString().toDouble(), 0.0)).toString()
@@ -143,7 +136,7 @@ class MainActivity : AppCompatActivity() {
         sqrtButton.view.setOnClickListener {
             val ops = getOps()
             if (sqrtButton.oneOpIsEmpty(ops[0]))
-                Toast.makeText(this, "Please enter value in operand 1", Toast.LENGTH_SHORT).show()
+                ToastOneOps(this)
             else {
                 result.text =
                     (sqrtButton.getResult(ops[0].text.toString().toDouble(), 0.0)).toString()
@@ -174,6 +167,15 @@ class MainActivity : AppCompatActivity() {
         return true
     }
 
+    fun ToastTwoOps(context: Context) {
+        Toast.makeText(context, context.getString(R.string.two_ops), Toast.LENGTH_LONG).show()
+        return
+    }
+
+    fun ToastOneOps(context: Context) {
+        Toast.makeText(context, context.getString(R.string.one_ops), Toast.LENGTH_LONG).show()
+        return
+    }
 }
 
 // AButton has a view that will be assigned later, a results function that will be assigned later
